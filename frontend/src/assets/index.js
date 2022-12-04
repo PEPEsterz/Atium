f60838fed8f49cca8ffedbc140947f03630c5178
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2022-12-04 13:25:26 +0100

    Add svg files

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,5 @@
+export { default as TWLogoBlack } from "./tw-logo-black.svg";
+export {default as SavingLogo } from "./savings.svg";
+export {default as SavingIntro } from "./saving-success.svg";
+export {default as Allowee } from "./allowance.svg";
+export {default as Gifting } from "./gifts.svg";
